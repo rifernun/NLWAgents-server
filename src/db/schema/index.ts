6cd0f9e1d45e89e34e433e041cc6c303752333da
Richard PC <rifernun@gmail.com>
2025-07-08 14:47:52 -0300

    feat: add create question, create room and get room questions

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,7 +1,8 @@
 //Barrow File - arquivo que exporta todos os arquivos que existem nesse contexto
 
+import { questions } from "./questions.ts";
 import { rooms } from "./rooms.ts";
 
 export const schema = {
-  rooms,
+  rooms, questions
 }
--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,9 @@ import { fastifyCors } from '@fastify/cors'
 import { env } from './env.ts'
 import { sql } from './db/connection.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { createRoomsRoute } from './http/routes/create-room.ts'
+import { getRoomsQuestions } from './http/routes/get-room-questions.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -23,5 +26,8 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(createRoomsRoute)
+app.register(getRoomsQuestions)
+app.register(createQuestionRoute)
 
 app.listen({ port: env.PORT })
