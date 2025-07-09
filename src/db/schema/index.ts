b3439c81bd39fd701bc9ea8b45cf736fc1b7c303
Richard PC <rifernun@gmail.com>
2025-07-09 17:23:19 -0300

    feat: gemini api integration and vector search

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,8 +1,9 @@
 //Barrow File - arquivo que exporta todos os arquivos que existem nesse contexto
 
+import { audioChunks } from "./audio-chunks.ts";
 import { questions } from "./questions.ts";
 import { rooms } from "./rooms.ts";
 
 export const schema = {
-  rooms, questions
+  rooms, questions, audioChunks
 }
--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,22 @@ import {
     type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
 import { env } from './env.ts'
 import { sql } from './db/connection.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 import { createRoomsRoute } from './http/routes/create-room.ts'
 import { getRoomsQuestions } from './http/routes/get-room-questions.ts'
 import { createQuestionRoute } from './http/routes/create-question.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
     origin: 'http://localhost:5173',
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -29,5 +33,6 @@ app.register(getRoomsRoute)
 app.register(createRoomsRoute)
 app.register(getRoomsQuestions)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT })
